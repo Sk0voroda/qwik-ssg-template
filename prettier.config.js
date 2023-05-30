06b530d2463d03fa7f86e462948d9a60ea001ff6
Shubert <103430520+Sk0voroda@users.noreply.github.com>
2023-05-30 14:09:44 +0300

    eslint

--- prettier.config.js
+++ prettier.config.js
@@ -3,10 +3,10 @@ const config = {
   printWidth: 80,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   semi: true,
-  arrowParens: "always",
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  arrowParens: 'always',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
 
 module.exports = config;
